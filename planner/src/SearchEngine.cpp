b5ac41241b29bdb7b536ea49544ccc65c5481b4f
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-06-29 12:24:09 +1000

    fix the segmentation fault for config-agile-3

--- planner/src/SearchEngine.cpp
+++ planner/src/SearchEngine.cpp
@@ -554,7 +554,7 @@ int main(int argc, char *argv[]) {
         searchNode* sol = search.search(
                 htn, tnI, timeL,
                 suboptimalSearch,
-                true,
+                false,
                 false,
                 heuristics,
                 hLength, visi, fringe);
